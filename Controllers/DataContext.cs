8373aaa65073d85090f943a2b5349d5cb8cc2421
Esteban Mesple-Somps <91217638+Spartanms64@users.noreply.github.com>
2023-02-09 16:24:39 +0100

    1

---
+++ Controllers/DataContext.cs
@@ -0,0 +1,20 @@
+﻿#region Import
+
+using Backend_Ressource_Relationnel.Models;
+using Microsoft.EntityFrameworkCore;
+#endregion
+namespace Backend_Ressource_Relationnel.Controllers
+{
+    // Classe representant la BDD
+    public class DataContext : DbContext
+    {
+        //**** 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysql-ressourcesre.alwaysdata.net;database=ressourcesre_bdd;user=299632;password=cda2022");
+        }
+
+
+    }
+}
