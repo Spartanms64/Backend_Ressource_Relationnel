9d9ebcdde3c5fb79b20e47b375878413eb215404
Esteban Mesple-Somps <91217638+Spartanms64@users.noreply.github.com>
2023-02-10 16:44:39 +0100

    t

--- Controllers/DataContext.cs
+++ Controllers/DataContext.cs
@@ -9,29 +9,41 @@ namespace Backend_Ressource_Relationnel.Controllers
     public class DataContext : DbContext
     {
 
-        DbSet<Category> categories { get; set; }
-        DbSet<User> users { get; set; }
-        DbSet<Role> roles { get; set; }
-        DbSet<Comment> comments { get; set; }
-        DbSet<Favorite> favorite { get; set; }
-        DbSet<Relation> relations { get; set; }
-        DbSet<Ressource> ressources { get; set; }
-        DbSet<Models.Type> types { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Role> roles { get; set; }
+        public DbSet<Comment> comments { get; set; }
+        public DbSet<Favorite> favorite { get; set; }
+        public DbSet<Relation> relations { get; set; }
+        public DbSet<Ressource> ressources { get; set; }
+        public DbSet<Models.Type> types { get; set; }
 
-
-
+        
         //**** Option de connexion BDD ****//
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySQL("server=mysql-ressourcesre.alwaysdata.net;database=ressourcesre_bdd;user=299632;password=cda2022;Encrypt=true;");
         }
 
-        //protected override void 
+        //****** Mise à jour classe + BDD *****//
 
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().ToTable("category");
+            modelBuilder.Entity<User>().ToTable("user");
+            modelBuilder.Entity<Role>().ToTable("role");
+            modelBuilder.Entity<Comment>().ToTable("comment");
+            modelBuilder.Entity<Favorite>().ToTable("favorite");
+            modelBuilder.Entity<Relation>().ToTable("relation");
+            modelBuilder.Entity<Ressource>().ToTable("ressource");
+            modelBuilder.Entity<Models.Type>().ToTable("model");
+        }
 
     }
 }
 ////////// Documentation
 ///https://learn.microsoft.com/en-us/ef/core/managing-schemas/scaffolding/?tabs=dotnet-core-cli
+///https://dev.mysql.com/doc/connector-net/en/connector-net-entityframework-core.html
+///https://learn.microsoft.com/en-us/ef/core/
+///https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-7.0&tabs=visual-studio
 /////////
